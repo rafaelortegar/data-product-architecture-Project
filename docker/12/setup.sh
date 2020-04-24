58607fea03dd4a3e66edc0d5f0f087fc26faf9ae
123972 <rafaelortega_itam@hotmail.com>
2020-04-24 07:06:24 -0500

    adding docker container

---
+++ docker/12/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+POSTGRESQL_VERSION=12
+sudo service ssh restart
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && echo "deb http://apt.postgresql.org/pub/repos/apt/ $RELEASE-pgdg main" | sudo tee  /etc/apt/sources.list.d/pgdg.list && sudo apt-get update && sudo apt-get -y install postgresql-$POSTGRESQL_VERSION
+
+sudo /etc/init.d/postgresql start
+sudo echo "host all  all    0.0.0.0/0  md5" >> /etc/docker/$POSTGRESQL_VERSION/main/pg_hba.conf
+sudo echo "local all all md5" >> /etc/docker/$POSTGRESQL_VERSION/main/pg_hba.conf
+sudo echo "listen_addresses='*'" >> /etc/docker/$POSTGRESQL_VERSION/main/postgresql.conf
+psql --command "ALTER USER metro WITH PASSWORD 'postgres';"
+sudo /etc/init.d/postgresql stop
+sudo pg_ctlcluster $POSTGRESQL_VERSION main start --foreground
