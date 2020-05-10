338fc471f02c40745ba767e2e2ebcfe109201486
123972 <rafaelortega_itam@hotmail.com>
2020-05-10 18:34:32 -0500

    adding lines

--- src/luigi/luigi_mas_completo.py
+++ src/luigi/luigi_mas_completo.py
@@ -38,8 +38,8 @@ class extractToJson(luigi.Task):
     def run(self): 
         creds_aws = pd.read_csv("../../credentials/credentials.csv")
         ses = boto3.session.Session(profile_name='rafael-dpa-proj') #, region='us-west-2') #profile_name='rafael-dpa-proj', region_name='us-west-2') # Pasamos los parámetros apra la creación del recurso S3 (bucket) al que se va a conectar
-        s3_resource = ses.resource('s3', aws_access_key_id=creds_aws.aws_access_key_id[0],
-                            aws_secret_access_key=creds_aws.aws_secret_access_key[0]) #Inicialzamos e recursoS3
+        s3_resource = ses.resource('s3') # , aws_access_key_id=creds_aws.aws_access_key_id[0],
+                            # aws_secret_access_key=creds_aws.aws_secret_access_key[0]) #Inicialzamos e recursoS3
         obj = s3_resource.Bucket(self.bucket) # metemos el bucket S3 en una variable obj
 
         print("#...")
@@ -215,12 +215,12 @@ class createTables(luigi.Task):
         creds = pd.read_csv("../../credentials_postgres.csv")
         creds_aws = pd.read_csv("../../credentials.csv")
         session = boto3.Session(profile_name='rafael-dpa-proj')
-        dev_s3_client = session.client('s3', aws_access_key_id=creds_aws.aws_access_key_id[0],
-                            aws_secret_access_key=creds_aws.aws_secret_access_key[0])
+        dev_s3_client = session.client('s3') # , aws_access_key_id=creds_aws.aws_access_key_id[0],
+                            # aws_secret_access_key=creds_aws.aws_secret_access_key[0])
         creds_aws = pd.read_csv("../../credentials.csv")
         print("Iniciando la conexión con el recurso S3 que contiene los datos extraídos...")
-        s3 = boto3.resource('s3', aws_access_key_id=creds_aws.aws_access_key_id[0],
-                            aws_secret_access_key=creds_aws.aws_secret_access_key[0])
+        s3 = boto3.resource('s3') # , aws_access_key_id=creds_aws.aws_access_key_id[0],
+                            # aws_secret_access_key=creds_aws.aws_secret_access_key[0])
 
         
         connection = psycopg2.connect(user=creds.user[0],
@@ -287,10 +287,10 @@ class createTables(luigi.Task):
         print("Schemas y tablas creados correctamente :)")
         
     
-    def output(self):
-        output_path = "s3://{}/{}/metro_{}_create_Table.txt". \
-            format(self.bucket, self.task_name, self.date) #Formato del nombre para el json que entra al bucket S3
-        return luigi.contrib.s3.S3Target(path=output_path)
+#    def output(self):
+#        output_path = "s3://{}/{}/metro_{}_create_Table.txt". \
+#            format(self.bucket, self.task_name, self.date) #Formato del nombre para el json que entra al bucket S3
+#        return luigi.contrib.s3.S3Target(path=output_path)
 
 #        is_complete = self.complete()
 #
@@ -365,10 +365,10 @@ class copyToPostgres(luigi.Task):
         connection.close()
         print("Carga de datos a la instancia RDS completada :)")
     
-    def output(self):
-        output_path = "s3://{}/{}/metro_{}_copyToPostgres.txt". \
-            format(self.bucket, self.task_name, self.date) #Formato del nombre para el json que entra al bucket S3
-        return luigi.contrib.s3.S3Target(path=output_path)
+#    def output(self):
+#        output_path = "s3://{}/{}/metro_{}_copyToPostgres.txt". \
+#            format(self.bucket, self.task_name, self.date) #Formato del nombre para el json que entra al bucket S3
+#        return luigi.contrib.s3.S3Target(path=output_path)
 #        is_complete = self.complete()
 #
 #    def complete(self):
