3bc1be125bef272a2c686f83752e0bbbf338b8bc
123972 <rafaelortega_itam@hotmail.com>
2020-05-10 22:06:57 -0500

    adding lines

--- src/luigi/luigi_mas_completo.py
+++ src/luigi/luigi_mas_completo.py
@@ -362,6 +362,8 @@ class copyToPostgres(luigi.Task):
             row_df.columns = ["fecha", "anio", "linea", "estacion", "afluencia"]
             df = pd.concat([df, row_df], ignore_index=True)
         
+        filas_a_cargar =len(df)
+        
         print("Inicia la conexión con la base de datos correspondiente en RDS...")
         connection = psycopg2.connect(user=creds.user[0],
                                       password=creds.password[0],
@@ -384,6 +386,11 @@ class copyToPostgres(luigi.Task):
     
     def output(self):
         return luigi.LocalTarget('1.ETL_copyToPosgres.txt')
+
+    def output(self):
+        output_path = "//{}/{}/metro_{}.json". \
+            format(self.bucket, self.task_name, self.date) #Formato del nombre para el json que entra al bucket S3
+        return luigi.contrib.s3.S3Target(path=output_path)
     
 #    def output(self):
 #        output_path = "s3://{}/{}/metro_{}_copyToPostgres.txt". \
@@ -405,33 +412,164 @@ class Metadata_load(luigi.Task):
     """
     task_name = 'raw_api'
     date = luigi.Parameter()
-    bucket = luigi.Parameter(default='dpaprojs3')
-    
-    #Dado que este metadata hace referencia a la carga de datos en el bucket s3, requiere que se haya finalizado la ejecución de CopyToPostgres
+    bucket = luigi.Parameter() # default='dpaprojs3')
+
+    # Indica que para iniciar el proceso de carga de metadatos requiere que el task de extractToJson esté terminado
     def requires(self):
         return copyToPostgres(bucket=self.bucket, date=self.date)
 
-    #Obtiene los metadatos de la carga de archivos a la postgres y los carga a metadata_load
+    # Esta sección indica lo que se va a correr:
     def run(self):
+        print("#...")
+        print("##...")
+        print("###...")
+        print("####...")
+        print("#####...")
+        print("######...")
+        print("Inicia la carga de los metadatos del extract...")
+        # Lee nuevamente el archivo JSON que se subió al S3 bucket, para después obtener metadatos sobre la carga
+        file_to_read = self.task_name + '/metro_' + self.date + '.json'
+        #creds = pd.read_csv("../../credentials_postgres.csv")
+        #session = boto3.Session(profile_name='default')
+
+        #Lee las credenciales de los archivos correspondientes
+        #session = boto3.Session(profile_name='rafael-dpa-proj')
+        creds = pd.read_csv("../../credentials_postgres.csv")
+        #creds_aws = pd.read_csv("../../credentials/credentials.csv")
+
+        creds_aws = pd.read_csv("../../credentials.csv")
+        ses = boto3.session.Session(profile_name='default') #, region='us-west-2') #profile_name='rafael-dpa-proj', region_name='us-west-2') # Pasamos los parámetros apra la creación del recurso S3 (bucket) al que se va a conectar
+        s3_resource = ses.resource('s3') # , aws_access_key_id=creds_aws.aws_access_key_id[0],
+                            # aws_secret_access_key=creds_aws.aws_secret_access_key[0]) #Inicialzamos e recursoS3
+        obj = s3_resource.Bucket(self.bucket) # metemos el bucket S3 en una variable obj
+        s3 = boto3.resource('s3')
+
+        print("#...")
+        print("##...")
+        print("###...")
+        print("####...")
+        print("#####...")
+        print("######...")
+        print("Conectando al S3 Bucket...")
+        # Obtiene el acceso al S3 Bucket con las credenciales correspondientes. Utiliza la paquetería boto3
+        #s3 = boto3.resource('s3') # , aws_access_key_id=creds_aws.aws_access_key_id[0],
+                           # aws_secret_access_key=creds_aws.aws_secret_access_key[0])
+        #s3 = boto3.resource('s3')
         
-        creds = pd.read_csv("../../credentials/credentials_postgres.csv")
-        creds_aws = pd.read_csv("../../credentials/credentials.csv")
+        # Metemos el ec2 y el s3 actuales en un objeto, para poder obtener sus metadatos
+        clientEC2 = boto3.client('ec2')
+        clientS3 = boto3.client('s3')
+        print("Inicializados el EC2 y el S3")
 
-        cliente_rds = boto3.client('rds')
+        # El content object está especificando el objeto que se va a extraer del bucket S3
+        # (la carga que se acaba de hacer desde la API)
+        content_object = s3.Object(self.bucket, file_to_read)
+        print("s3 encontrada exitosamente")
 
+        # Esta línea lee el archivo especificado en content_object
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        print("contenido leído exitosamente")
+        # Carga el Json content desde el archivo leído de la S3 Bucket
+        json_content = json.loads(file_content)
+        print("contenido cargado exitosamente")
 
+        # Inicializa el data frame que se va a meter la información de los metadatos
+#        df = pd.DataFrame(columns=["fecha_ejecucion", "fecha_json", "usuario", "ip_ec2", "ruta_bucket", "status", "columns_read"])
+        
+        #función de EC2 para describir la instancia en la que se está trabajando
+        information_metadata_ours = clientEC2.describe_instances()
+        print("ec2 descrita correctamente")
+        
+        
+        
+        # Columns read indica la cantidad de columnas leidas
+        columns_loaded = len(json_content['records'])
+        fecha_ejecucion = pd.Timestamp.now()
+        user = information_metadata_ours.get('Reservations')[0].get('Instances')[0].get('KeyName')
+        fecha_json = self.date
+        ip_ec2 = information_metadata_ours.get('Reservations')[0].get('Instances')[0].get('PrivateIpAddress')
+        nombre_bucket = self.bucket
+        status = 'Loaded'
+        print("variables a cargar listas")
+        
+#        client.get('Reservations')[0].get('Instances')[0].get('KeyName')
+
+        print("#...")
+        print("##...")
+        print("###...")
+        print("####...")
+        print("#####...")
+        print("######...")
+        print("Conectandose a la instancia RDS con los datos RAW...")
+        #Se conecta a la postgres en el RDS con las credenciales correspondientes
         connection = psycopg2.connect(user=creds.user[0],
                                       password=creds.password[0],
                                       host=creds.host[0],
                                       port=creds.port[0],
                                       database=creds.db[0])
-        
-        
+
+
+        # Allows Python code to execute PostgreSQL command in a database session.
         cursor = connection.cursor()
-
-
-        df = psql.read_sql("SELECT * FROM raw.metadata_load;", connection)
         
+
+        # Inserta los metadatos en la tabla metadata_extract
+        text = "INSERT INTO raw.metadataload  VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (
+        user,fecha_ejecucion, fecha_json,ip_ec2, nombre_bucket, columns_loaded)
+        print(text)
+        
+        cursor.execute(text) #Execute a database operation (query or command).
+
+        
+        connection.commit() # This method sends a COMMIT statement to the MySQL server, committing the current transaction. 
+        cursor.close()# Close the cursor now (rather than whenever del is executed). The cursor will be unusable from this point forward
+        connection.close() # For a connection obtained from a connection pool, close() does not actually close it but returns it to the pool and makes it available for subsequent connection requests.
+        print("#...")
+        print("##...")
+        print("###...")
+        print("####...")
+        print("#####...")
+        print("######...")
+        print("Carga de metadatos de Extract completada! :)")
+
+    def output(self):
+        return luigi.LocalTarget('1.ETL_metadataLoad.txt')
+
+
+
+
+
+#    task_name = 'raw_api'
+#    date = luigi.Parameter()
+#    bucket = luigi.Parameter(default='dpaprojs3')
+#    
+#    #Dado que este metadata hace referencia a la carga de datos en el bucket s3, requiere que se haya finalizado la ejecución de CopyToPostgres
+#    def requires(self):
+#        return copyToPostgres(bucket=self.bucket, date=self.date)
+#
+#    #Obtiene los metadatos de la carga de archivos a la postgres y los carga a metadata_load
+#    def run(self):
+#        
+#        creds = pd.read_csv("../../credentials/credentials_postgres.csv")
+#        creds_aws = pd.read_csv("../../credentials/credentials.csv")
+#
+#        cliente_rds = boto3.client('rds')
+#
+#
+#        connection = psycopg2.connect(user=creds.user[0],
+#                                      password=creds.password[0],
+#                                      host=creds.host[0],
+#                                      port=creds.port[0],
+#                                      database=creds.db[0])
+#        
+#        
+#        cursor = connection.cursor()
+#
+#
+#
+#
+#        #df = psql.read_sql("SELECT * FROM raw.metadata_load;", connection)
+#        
         # Lee de raw postgresql
         #Guarda postgresql
         # Esquema Clean
@@ -461,46 +599,6 @@ class Metadata_load(luigi.Task):
 #        print("***** Failed getting df: {} *****".format(error))
 #
 #    return dataframe
-
-
-
-
-
-
-
-        file_to_read = self.task_name + '/metro_' + self.date + '.json'
-        creds = pd.read_csv("../../credentials/credentials_postgres.csv")
-        creds_aws = pd.read_csv("../../credentials/credentials.csv")
-        s3 = boto3.resource('s3', aws_access_key_id=creds_aws.Access_key_ID[0],
-                            aws_secret_access_key=creds_aws.Secret_access_key[0])
-        content_object = s3.Object(self.bucket, file_to_read)
-        file_content = content_object.get()['Body'].read().decode('utf-8')
-        json_content = json.loads(file_content)
-
-        df = pd.DataFrame(columns=["fecha", "anio", "linea", "estacion", "afluencia"])
-
-        for i in range(len(json_content['records'])):
-            a_row = pd.Series(
-                [json_content['records'][i]["fields"]["fecha"], json_content['records'][i]["fields"]["anio"],
-                 json_content['records'][i]["fields"]["linea"], json_content['records'][i]["fields"]["estacion"],
-                 int(json_content['records'][i]["fields"]["afluencia"])])
-            row_df = pd.DataFrame([a_row])
-            row_df.columns = ["fecha", "anio", "linea", "estacion", "afluencia"]
-            df = pd.concat([df, row_df], ignore_index=True)
-        connection = psycopg2.connect(user=creds.user[0],
-                                      password=creds.password[0],
-                                      host=creds.host[0],
-                                      port=creds.port[0],
-                                      database=creds.db[0])
-        cursor = connection.cursor()
-        for i in df.index:
-            text = "INSERT INTO raw  VALUES ('%s', '%s', '%s', '%s', %d);" % (
-            df["fecha"][i], df["anio"][i], df["linea"][i], df["estacion"][i], df["afluencia"][i])
-            print(text)
-            cursor.execute(text)
-        connection.commit()
-        cursor.close()
-        connection.close()
 
 
 ############################################################# RUN ALL TASK ####################################
