689ba10133d880a913f66c8f204894e2f8163ad2
123972 <rafaelortega_itam@hotmail.com>
2020-05-12 15:30:03 -0500

    add

--- src/luigi/luigi_mas_completo.py
+++ src/luigi/luigi_mas_completo.py
@@ -1044,9 +1044,9 @@ class featureEngineering(luigi.Task):
         #user,password,host,port,db
         #postgres,12345678,database-1.cqtrfcufxibu.us-west-2.rds.amazonaws.com,5432,dpa
 
-        table_name='metro'
+        table_name='semantic.metro'
         scheme='semantic'
-        df2.to_sql("metro", engine, schema='semantic',if_exists='replace')
+        df2.to_sql("metro", con=engine, schema='semantic',if_exists='replace')
         print(psql.read_sql('SELECT * FROM semantic.metro LIMIT 10;', connection))
         
         # para los outputs que no vamos a usar
@@ -1084,7 +1084,7 @@ class metadataFeatureEngineering(luigi.Task):
     #==============================================================================================================
     # Parameters
     #==============================================================================================================
-    task_name = 'metadata_feature_engineering_06_02'
+    task_name = 'metadata_feature_engineering_05_02'
     date = luigi.Parameter()
     bucket = luigi.Parameter(default='dpaprojs3') # default='dpaprojs3')
     #==============================================================================================================
@@ -1135,7 +1135,7 @@ class metadataFeatureEngineering(luigi.Task):
         print("s3 encontrada exitosamente")
 
         # Esta línea lee el archivo especificado en content_object
-        file_content = pd.read_csv(content_object)    # content_object.get()['Body'].read().decode('utf-8') # Esto está de más
+        #file_content = pd.read_csv(content_object)    # content_object.get()['Body'].read().decode('utf-8') # Esto está de más
         print("contenido leído exitosamente")
         # Carga el Json content desde el archivo leído de la S3 Bucket
         #json_content = json.loads(file_content) # Esto está de más
@@ -1145,14 +1145,14 @@ class metadataFeatureEngineering(luigi.Task):
         information_metadata_ours = clientEC2.describe_instances()
         print("ec2 descrita correctamente")
         
-        columnas_leidas = file_content # pd.read_csv('../../columnas_leidas.csv')
+        #columnas_leidas = file_content # pd.read_csv('../../columnas_leidas.csv')
         print("csv leido correctamente")
         
         
         # Columns read indica la cantidad de columnas leidas
-        columns_loaded = 196 # columnas_leidas['datos a cargar'][0]
-        print("se cargaron:", columns_loaded, " columnas.")
-        print(columns_loaded)
+        #columns_loaded = 196 # columnas_leidas['datos a cargar'][0]
+        #print("se cargaron:", columns_loaded, " columnas.")
+        #print(columns_loaded)
         fecha_ejecucion = pd.Timestamp.now()
         user = information_metadata_ours.get('Reservations')[0].get('Instances')[0].get('KeyName')
         fecha_json = self.date
@@ -1180,15 +1180,17 @@ class metadataFeatureEngineering(luigi.Task):
 
         # Allows Python code to execute PostgreSQL command in a database session.
         cursor = connection.cursor()
+        text1= "SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_catalog = 'dpa' AND table_name = 'semantic.metro'"
+        columnas_totales= cursor.execute(text1)
         
 
         # Inserta los metadatos en la tabla metadata_extract
         text = "INSERT INTO cleaned.metadata  VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (
-        user,fecha_ejecucion, fecha_json,ip_ec2, nombre_bucket, columns_loaded)
+        user,fecha_ejecucion, fecha_json,ip_ec2, nombre_bucket,columnas_totales) # , columns_loaded)
         print(text)
         
         cursor.execute(text) #Execute a database operation (query or command).
-
+        
         
         connection.commit() # This method sends a COMMIT statement to the MySQL server, committing the current transaction. 
         cursor.close()# Close the cursor now (rather than whenever del is executed). The cursor will be unusable from this point forward
@@ -1199,13 +1201,13 @@ class metadataFeatureEngineering(luigi.Task):
         print("####...")
         print("#####...")
         print("######...")
-        print("Carga de metadatos de Extract completada! :)")
+        print("Carga de metadatos de Feature Engineering completada! :)")
 
         # para los outputs que no vamos a usar
         vacio = ' '
         data_vacia = {'vacio':[vacio]}
         pandas_a_csv = pd.DataFrame(data=data_vacia)
-        pandas_a_csv(output().path, index=False)
+        pandas_a_csv.to_csv(self.output().path, index=False)
         print("archivo creado correctamente")
 
 
@@ -1216,11 +1218,102 @@ class metadataFeatureEngineering(luigi.Task):
         return luigi.contrib.s3.S3Target(path=output_path)
 
 
-
-
-
 ############################################################## EMPEZAR MODELADO ###################################
-#
+class modelingMetro(luigi.task):
+    """
+    Function to train model from the mexico city metro data set on the database on postgres.
+    It stores the metadata from uploading into the specified S3 bucket on AWS. Note: user MUST have the credentials 
+    to use the aws s3 bucket.
+    """
+
+    #==============================================================================================================
+    # Parameters
+    #==============================================================================================================
+    task_name = 'modelingMetro_task_06_01'
+    date = luigi.Parameter()
+    bucket = luigi.Parameter(default='dpaprojs3') # default='dpaprojs3')
+    #==============================================================================================================
+
+    # Indica que para iniciar el proceso de carga de metadatos requiere que emetadataloadl task de extractToJson esté terminado
+    def requires(self):
+        return featureEngineering(bucket=self.bucket, date=self.date), metadataFeatureEngineering(bucket=self.bucket, date=self.date)
+
+    
+    def run(self):
+
+        # Lee nuevamente el archivo JSON que se subió al S3 bucket, para después obtener metadatos sobre la carga
+        archivoquenosirve = 'feature_engineering_05_01/metro_' + self.date + '.csv'
+
+        creds = pd.read_csv("../../credentials_postgres.csv")
+        creds_aws = pd.read_csv("../../credentials.csv")
+        print("credenciales leídas correctamente")
+
+        # Conexión a la S3
+        print("Iniciando la conexión con el recurso S3 que contiene los datos extraídos...")
+        ses = boto3.session.Session(profile_name='rafael-dpa-proj') #, region_name='us-west-2') # Pasamos los parámetros apra la creación del recurso S3 (bucket) al que se va a conectar
+        s3_resource = ses.resource('s3') # Inicialzamos e recursoS3
+        obj = s3_resource.Bucket(self.bucket) # Metemos el bucket S3 en una variable obj
+        print("Conexión Exitosa! :)")
+
+        content_object = s3_resource.Object(self.bucket, archivoquenosirve)
+        #file_content = pd.read_csv(content_object) 
+        print("s3 encontrada exitosamente")
+
+
+        connection = psycopg2.connect(user=creds.user[0],
+                                          password=creds.password[0],
+                                          host=creds.host[0],
+                                          port=creds.port[0],
+                                          database=creds.db[0])
+        cursor = connection.cursor()
+        df = psql.read_sql('SELECT * FROM semantic.metro', connection)
+        #dummies=pd.get_dummies(df["ano"],prefix='y')
+        #df=pd.concat([df,dummies],axis=1)
+        #dummies=pd.get_dummies(df["linea"],prefix='l')
+        #df=pd.concat([df,dummies],axis=1)
+        #print(df.columns)
+
+        
+
+        print(df.shape)
+        #sqlalchemy engine to psycopg2
+        #dialect+driver://username:password@host:port/database
+        engine = create_engine('postgresql+psycopg2://postgres:12345678@database-1.cqtrfcufxibu.us-west-2.rds.amazonaws.com:5432/dpa')
+        #user,password,host,port,db
+        #postgres,12345678,database-1.cqtrfcufxibu.us-west-2.rds.amazonaws.com,5432,dpa
+
+        table_name='semantic.metro'
+        scheme='semantic'
+        #df.to_sql(name="semantic.metro", con=engine, schema=scheme,if_exists='replace')
+        #print(psql.read_sql('SELECT * FROM semantic.metro LIMIT 10;', connection))
+        
+        cursor.execute(text) #Execute a database operation (query or command).
+        
+        
+        connection.commit() # This method sends a COMMIT statement to the MySQL server, committing the current transaction. 
+        cursor.close()# Close the cursor now (rather than whenever del is executed). The cursor will be unusable from this point forward
+        connection.close() # For a connection obtained from a connection pool, close() does not actually close it but returns it to the pool and makes it available for subsequent connection requests.
+
+
+
+        # para los outputs que no vamos a usar
+        vacio = ' '
+        data_vacia = {'vacio':[vacio]}
+        pandas_a_csv = pd.DataFrame(data=data_vacia)
+        pandas_a_csv.to_csv(self.output().path, index=False)
+        print("archivo creado correctamente")    
+
+#=======
+#        df2.to_sql("metro", engine, schema='semantic',if_exists='replace')
+        
+
+    
+    # Envía el output al S3 bucket especificado con el nombre de output_path
+    def output(self):
+        output_path = "s3://{}/{}/metro_{}.csv". \
+            format(self.bucket, self.task_name, self.date) #Formato del nombre para el json que entra al bucket S3
+        return luigi.contrib.s3.S3Target(path=output_path)
+
 #class SeparaBase(luigi.Task):
 #    "Esta tarea separa la base en la Train & Test"
 #
@@ -1464,7 +1557,7 @@ class runAll(luigi.WrapperTask):
     #==============================================================================================================
 
     def requires(self):
-        yield featureEngineering(bucket=self.bucket, date=self.date)
+        yield metadataFeatureEngineering(bucket=self.bucket, date=self.date)
 
 
 
