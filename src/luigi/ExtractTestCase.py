c6a6930de54d50574ddf891c77c23c18125f4280
123972 <rafaelortega_itam@hotmail.com>
2020-05-12 19:43:47 -0500

    adding

--- src/luigi/ExtractTestCase.py
+++ src/luigi/ExtractTestCase.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import marbles.core
 import pandas as pd
+import sys
 #directorio = '/home/alfie-gonzalez/Documentos/Maestría/Segundo Semestre/Métodos de Gran Escala'
 #os.chdir(directorio)
 #file = 'afluencia-diaria-del-metro-cdmx.json'
@@ -18,7 +19,7 @@ import pandas as pd
 class ExtractTestCase(marbles.core.TestCase):
         def setUp(self):
                 self.json_file = json_file   # ... #json_file
-                self.date=date
+                #self.date=date
                 #self.json_file = pd_json
         #def __init__(self):
         #        self.json_file
@@ -29,33 +30,35 @@ class ExtractTestCase(marbles.core.TestCase):
         def test_extract(self):
                 #df = pd.read_json(r'contenido.json',encoding='utf-8', orient='values', lines=True)
                 #cuenta=df['nhits'][0]
-                self.assertTrue(self.json_file.shape[0] != 0, note = 'json file is empty')
+                #self.assertTrue(self.json_file.shape[0] != 0, note = 'json file is empty')
+                self.assertTrue(sys.getsizeof(self.json_file) != 0, note = 'json file is empty')
                 #self.assertTrue(self.json_file.shape <= 0, note = 'json file is empty')
                 now = datetime.datetime.now()
-                passfail=self.json_file.read(2) != ''
+                #passfail=self.json_file.read(2) != ''
+                passfail=sys.getsizeof(self.json_file) != 0
                 #passfail=self.json_file.shape[0] != 0
                 nombreprueba='test load datos raw'
                 print (now.strftime("%Y-%m-%d %H:%M:%S"))
 
-                #if passfail==True:
-                #        print("#############")
-                #        print("prueba exitosa")
-                #        print("#############")
-                #else:
-                #        print("############")
-                #        print("prueba fallida")
-                #        print("#############")
-
-                if self.date<now:
+                if passfail==True:
                         print("#############")
                         print("prueba exitosa")
                         print("#############")
                 else:
-                        print("#############")
+                        print("############")
                         print("prueba fallida")
                         print("#############")
 
-                return None # print(passfail)
+                #if self.date<now:
+                #        print("#############")
+                #        print("prueba exitosa")
+                #        print("#############")
+                #else:
+                #        print("#############")
+                #        print("prueba fallida")
+                #        print("#############")
+#
+                return print(passfail)
                 #df1 = pd.DataFrame({'prueba':nombreprueba, 'estatus':passfail, 'hora_ejecucion':now})
                 #return df1.head()
 
