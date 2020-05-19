8428c7bb2f5f5b7a8a0b9f9cc7755810f2fb1824
123972 <rafaelortega_itam@hotmail.com>
2020-05-19 05:34:55 -0500

    cambio en la lista

--- src/luigi/Version2/copyToPostgres.py
+++ src/luigi/Version2/copyToPostgres.py
@@ -51,12 +51,13 @@ class copyToPostgres(CopyToTable):
 #                tupla_ingresar = (data['records'][line].get('fields').get('fecha'),data['records'][line].get('fields').get('anio'),
 #                                  data['records'][line].get('fields').get('linea'),data['records'][line].get('fields').get('estacion'),
 #                                  data['records'][line].get('fields').get('afluencia'))
-                fecha_ingreso = data['records'][line].get('fields').get('fecha')
-                anio_ingreso = data['records'][line].get('fields').get('anio')
-                linea_ingreso = data['records'][line].get('fields').get('linea')
-                estacion_ingreso = data['records'][line].get('fields').get('estacion')
-                afluencia_ingreso = data['records'][line].get('fields').get('afluencia')
-                yield (fecha_ingreso,anio_ingreso,linea_ingreso,estacion_ingreso,afluencia_ingreso)
+                #fecha_ingreso = data['records'][line].get('fields').get('fecha')
+                fecha_ingreso = line.get('fields').get('fecha')
+#                anio_ingreso = data['records'][line].get('fields').get('anio')
+#                linea_ingreso = data['records'][line].get('fields').get('linea')
+#                estacion_ingreso = data['records'][line].get('fields').get('estacion')
+#                afluencia_ingreso = data['records'][line].get('fields').get('afluencia')
+                yield (fecha_ingreso) #,anio_ingreso,linea_ingreso,estacion_ingreso,afluencia_ingreso)
                 yield line.strip('\n').split('\t')
         
 #        with open('metro_2019-01-01.json') as json_file: 
@@ -64,6 +65,10 @@ class copyToPostgres(CopyToTable):
 #            data['records'][0].get('fields')
 #            len(data['records'])
 
+#data['records'][line].get('fields').get('fecha')
+#>>> data['records'][0]
+{'datasetid': 'afluencia-diaria-del-metro-cdmx', 'recordid': 'ec33a8dcf83dba40ce9b09e1f51f67acefe1a0ef', 'fields': {'anio': '2019',
+  'estacion': 'Pantitlán', 'fecha': '2019-01-01', 'linea': 'Linea 1', 'afluencia': '14452'}, 'record_timestamp': '2020-03-25T17:13:11.354000+00:00'}
 
 
 #    def run(self):
