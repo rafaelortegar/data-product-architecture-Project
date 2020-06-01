7b24ec5fdb183782d6002618785342db1585ddb9
123972 <rafaelortega_itam@hotmail.com>
2020-05-31 21:45:01 -0500

    fixing testload1

--- src/luigi/Version2/funciones_rds.py
+++ src/luigi/Version2/funciones_rds.py
@@ -9,12 +9,12 @@ class conectaAtablaRawMetro(object):
     
     def __init__(self,host,database,user,password,table,port):
         creds = pd.read_csv("../../../credentials_postgres.csv")
-        self.host = creds.host[0]
-        self.database = creds.database[0]
-        self.user = creds.user[0]
-        self.password = creds.password[0]
-        self.table = 'raw.metro' 
-        self.port = creds.port[0]
+        host = creds.host[0]
+        database = creds.database[0]
+        user = creds.user[0]
+        password = creds.password[0]
+        table = 'raw.metro' 
+        port = creds.port[0]
         
         
     def dbaspandas(self):
