ec3fedd8c00f9de025159214f3b3ec98f12fc2b6
Javier Valencia Goujon <valencig@hotmail.com>
2020-06-01 16:44:57 -0500

    corregi error de indentacion

--- src/luigi/Version2/loadCleaned.py
+++ src/luigi/Version2/loadCleaned.py
@@ -11,38 +11,38 @@ from loadCleaned import loadCleaned
 logger = logging.getLogger('luigi-interface')
 
 class loadCleaned(PostgresQuery):
-#==============================================================================================================
-# Parameters
-#==============================================================================================================
-task_name='cleaned_data_03_01'
-date = luigi.Parameter()
-bucket = luigi.Parameter(default='dpaprojs3')
-#==============================================================================================================
-# Parameters for database connection
-#==============================================================================================================
-creds = pd.read_csv("../../../credentials_postgres.csv")
-creds_aws = pd.read_csv("../../../credentials.csv")
-print('Credenciales leídas correctamente')
-host = creds.host[0]
-database = creds.db[0]
-user = creds.user[0]
-password = creds.password[0]
-table = 'cleaned.metro'
-port = creds.port[0]
-query = """
-    drop table if exists cleaned.metro cascade;
-    create table cleaned.metro as (
-        SELECT 
-        fecha::DATE as fecha, 
-        anio::int as ano, 
-        linea::varchar as linea, 
-        estacion::varchar as estacion,
-        afluencia::int as afluencia
-        from raw.metro
-        );  
-        """ 
+    #==============================================================================================================
+    # Parameters
+    #==============================================================================================================
+    task_name='cleaned_data_03_01'
+    date = luigi.Parameter()
+    bucket = luigi.Parameter(default='dpaprojs3')
+    #==============================================================================================================
+    # Parameters for database connection
+    #==============================================================================================================
+    creds = pd.read_csv("../../../credentials_postgres.csv")
+    creds_aws = pd.read_csv("../../../credentials.csv")
+    print('Credenciales leídas correctamente')
+    host = creds.host[0]
+    database = creds.db[0]
+    user = creds.user[0]
+    password = creds.password[0]
+    table = 'cleaned.metro'
+    port = creds.port[0]
+    query = """
+        drop table if exists cleaned.metro cascade;
+        create table cleaned.metro as (
+            SELECT 
+            fecha::DATE as fecha, 
+            anio::int as ano, 
+            linea::varchar as linea, 
+            estacion::varchar as estacion,
+            afluencia::int as afluencia
+            from raw.metro
+            );  
+            """ 
 
-#=============================================================================================================
+    #=============================================================================================================
     def requires(self):
         return copyToPostgres(bucket=self.bucket, date=self.date)
     
