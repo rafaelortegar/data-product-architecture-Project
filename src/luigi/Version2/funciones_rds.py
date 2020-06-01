9befedd3d1fb772668b8468cb91d11ad287c0e81
123972 <rafaelortega_itam@hotmail.com>
2020-05-31 21:48:47 -0500

    fixing testload1self

--- src/luigi/Version2/funciones_rds.py
+++ src/luigi/Version2/funciones_rds.py
@@ -5,16 +5,24 @@ import pandas as pd
 import pandas.io.sql as psql
 from sqlalchemy import create_engine
 
+creds = pd.read_csv("../../../credentials_postgres.csv")
+host = creds.host[0]
+database = creds.database[0]
+user = creds.user[0]
+password = creds.password[0]
+table = 'raw.metro' 
+port = creds.port[0]
+
 class conectaAtablaRawMetro(object):
     
     def __init__(self,host,database,user,password,table,port):
         creds = pd.read_csv("../../../credentials_postgres.csv")
-        host = creds.host[0]
-        database = creds.database[0]
-        user = creds.user[0]
-        password = creds.password[0]
-        table = 'raw.metro' 
-        port = creds.port[0]
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.table = table
+        self.port = port
         
         
     def dbaspandas(self):
