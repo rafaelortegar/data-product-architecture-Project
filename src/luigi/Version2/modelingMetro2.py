4cc3fc1ea0e850832db1422ac3cca9616d082f46
123972 <rafaelortega_itam@hotmail.com>
2020-06-02 06:01:42 -0500

    pikle5

--- src/luigi/Version2/modelingMetro2.py
+++ src/luigi/Version2/modelingMetro2.py
@@ -88,8 +88,8 @@ class modelingMetro2(luigi.Task):
 #        pickle.dump(modelos, file)
 #        file.close()
 #        
-        with self.output().open('w') as output_file:
-            pickle.dump(modelos,output_file)
+        with self.output().open('w') as output_path:
+            pickle.dump(modelos,output_path)
         
         print("#...")
         print("##...")
@@ -111,8 +111,7 @@ class modelingMetro2(luigi.Task):
         
     # Envía el output al S3 cop especificado con el nombre de output_path
     def output(self):
-        output_path = "s3://dpaprojs3/modelingMetro_task_06_01/metro_{}.pkl". \
-            format(self.date) #Formato del nombre para el json que entra al bucket S3
+        output_path = "s3://dpaprojs3/modelingMetro_task_06_01/metro_{}.pkl".format(self.date) #Formato del nombre para el json que entra al bucket S3
         return luigi.contrib.s3.S3Target(path=output_path)
 
 
