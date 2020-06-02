09cb3e2e0ead89ef787937191d21b9bb739b04aa
123972 <rafaelortega_itam@hotmail.com>
2020-06-02 01:29:08 -0500

    verificando algo

--- src/luigi/Version2/copyToPostgres.py
+++ src/luigi/Version2/copyToPostgres.py
@@ -44,7 +44,7 @@ class copyToPostgres(CopyToTable):
     def requires(self):
         def _requires(self):
             return metadataExtract(bucket=self.bucket,date=self.date)
-        return extractToJson(bucket=self.bucket, date=self.date) # , metadataExtract(bucket=self.bucket, date=self.date) # , testExtract(bucket=self.bucket, date=self.date), metadataTestExtract(bucket=self.bucket, date=self.date)
+        return extractToJson(bucket=self.bucket, date=self.date),metadataExtract(bucket=self.bucket, date=self.date) # , metadataExtract(bucket=self.bucket, date=self.date) # , testExtract(bucket=self.bucket, date=self.date), metadataTestExtract(bucket=self.bucket, date=self.date)
 #        def _requires(self):
 #            return metadataExtract(bucket=self.bucket,date=self.date)
 
