ea3dabce7d3fa9af9b054f8aa80476af69854590
123972 <rafaelortega_itam@hotmail.com>
2020-06-02 07:48:00 -0500

    output

--- src/luigi/Version2/predictionMetro.py
+++ src/luigi/Version2/predictionMetro.py
@@ -96,7 +96,8 @@ class predictionMetro(luigi.Task):
         pandas_a_csv = pd.DataFrame(data=datos_a_csv)
         print("dimensiones de prediccion pandas_a_csv",pandas_a_csv.shape)
         
-        pandas_a_csv.to_csv(path=self.output().path, index=False)
+        print(self.output.path)
+        pandas_a_csv.to_csv(self.output().path, index=False)
         
         
         engine = create_engine('postgresql+psycopg2://postgres:12345678@database-1.cqtrfcufxibu.us-west-2.rds.amazonaws.com:5432/dpa')
@@ -115,6 +116,7 @@ class predictionMetro(luigi.Task):
     # Envía el output al S3 cop especificado con el nombre de output_path
     def output(self):
         output_path = "s3://dpaprojs3/predictionMetro_task_07_01/metro_{}.csv".format(self.date) #Formato del nombre para el json que entra al bucket S3
+        print("en el output:",output_path)
         return luigi.contrib.s3.S3Target(path=output_path)
 
 
