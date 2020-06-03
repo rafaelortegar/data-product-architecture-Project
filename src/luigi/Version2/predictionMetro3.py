707ca09021f2db3112b97cb863b783cc0ab18bb9
123972 <rafaelortega_itam@hotmail.com>
2020-06-02 22:28:42 -0500

    bascamodelo

---
+++ src/luigi/Version2/predictionMetro3.py
@@ -0,0 +1,187 @@
+import requests
+import luigi
+import logging
+import psycopg2
+import sqlalchemy
+import pickle
+import boto3
+import boto3.session
+from io import BytesIO
+
+
+import pandas.io.sql as psql
+import pandas as pd
+
+from sqlalchemy import create_engine
+from luigi.contrib.s3 import S3Target
+
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+from featureEngineering2 import featureEngineering2
+
+from modelingMetro3 import modelingMetro3
+from metadataModeling import metadataModeling
+
+import modelado
+import prediction
+from feature_builder import FeatureBuilder
+
+
+
+class predictionMetro(luigi.Task):
+    """
+    Falta el comentario
+    """
+
+    #==============================================================================================================
+    # Parameters
+    #==============================================================================================================
+    task_name = 'predictionMetro_task_07_01'
+    date = luigi.Parameter()
+    bucket = luigi.Parameter(default='dpaprojs3') # default='dpaprojs3')
+    #==============================================================================================================
+    
+    # Indica que para iniciar loadCleaned proceso de carga de metadatos requiere que el task de extractToJson esté terminado
+    def requires(self):
+        return metadataModeling(bucket=self.bucket, date=self.date) # , metadataCleaned(bucket = self.bucket, date=  self.date)
+
+    
+
+    def run(self):
+        creds = pd.read_csv("../../../credentials_postgres.csv")
+        creds_aws = pd.read_csv("../../../credentials.csv")
+        
+        connection = psycopg2.connect(user=creds.user[0],
+                                          password=creds.password[0],
+                                          host=creds.host[0],
+                                          port=creds.port[0],
+                                          database=creds.db[0])
+        cursor = connection.cursor()
+        df = psql.read_sql('SELECT * FROM modeling.metadata ORDER BY accuracy_promedio DESC LIMIT 1 ;', connection)
+        cursor.close()
+        
+        modelobuscado = df['nombre_modelo'][0]
+        
+        # Conectamos al bucket
+        ses = boto3.session.Session(profile_name='rafael-dpa-proj') # , region_name='us-west-2') # Pasamos los parámetros apra la creación del recurso S3 (bucket) al que se va a conectar
+        s3_resource = ses.resource('s3')
+        obj = s3_resource.Bucket(self.bucket) # metemos el bucket S3 en una variable obj
+        dev_s3_client = ses.client('s3')
+        #modelobuscado = "modelingMetro_task_06_01/metro_{}.pkl".format(self.date)
+        print(modelobuscado)
+        with BytesIO() as data:
+            s3_resource.Bucket(self.bucket).download_fileobj(modelobuscado,data)
+            data.seek(0)
+            model = pickle.load(data)
+        print("Voy a leer el pikle de la s3")
+        print(model)
+        #pickledesdeS3 = self.input().open('r')
+        #print(pickledesdeS3)
+        #archivopickle = open(self.input(), 'rb')
+        #archivopickle = open(pickledesdeS3, 'r')
+        print("el archivopikle es:")
+        #print(archivopickle)
+        
+        print("cargando el pikle")
+        #loadpickleours = pickle.load(archivopickle)
+        
+        #modelospikledes3 = loadpickleours.copy()
+        #print(modelospikledes3)
+        # conectamos a la RDS
+#        connection = psycopg2.connect(user=creds.user[0],
+#                                  password=creds.password[0],
+#                                  host=creds.host[0],
+#                                  port=creds.port[0],
+#                                  database=creds.db[0])
+#        
+#        cursor = connection.cursor()
+        
+        # Leemos los datos de la RDS
+        #df = psql.read_sql('SELECT * FROM semantic.metro;', connection)
+        #print(df.shape) 
+        ##########################################################
+        ## Lee nuevamente el archivo JSON que se subió al S3 bucket, para después obtener metadatos sobre la carga
+        ##file_to_read = 'extractToJson_task_01/metro_'+ self.date +'.json'
+        #file_to_read ="s3://dpaprojs3/modelingMetro_task_06_01/metro_{}.pkl".format(self.date)
+        #print("El archivo a buscar es: ",file_to_read)
+
+        ## Conexión a la S3
+        #ses = boto3.session.Session(profile_name='rafael-dpa-proj') #, region_name='us-west-2') # Pasamos los parámetros apra la creación del recurso S3 (bucket) al que se va a conectar
+        #s3_resource = ses.resource('s3') # Inicialzamos e recursoS3
+        #obj = s3_resource.Bucket(self.bucket) # Metemos el bucket S3 en una variable obj
+        #dev_s3_client = ses.client('s3')
+
+        #with BytesIO() as data:
+        #    s3_resource.Bucket(self.bucket).download_fileobj(file_to_read,data)
+        #    data.seek(0)
+        #    modelos = pickle.load(data)
+
+        ##content_object = dev_s3_client.get_object(Bucket=self.bucket,Key='s3://dpaprojs3/modelingMetro_task_06_01/metro_'+self.date+'.pkl')
+        #print("s3 encontrada exitosamente")
+        ##########################################################
+        # Cargamos el modelo
+        file = open('modelo.pkl', 'rb')
+        #file = open(content_object, 'rb')
+        data = pickle.load(file)
+        file.close()
+        modelos = data.copy()
+        
+        df = pd.read_csv('x_original.csv')
+        print("shape de lo leido",df.shape)
+        #        fecha = '2020-06-02'
+        
+        fecha = self.date
+        
+        pred = prediction.Predict()
+        print(pred)
+        pred = pred.predict(fecha, df, model)
+        print(pred)
+        print(pred.shape)
+        
+        # para los outputs que no vamos a usar
+        
+        #fecha = pred['fecha'][0]
+        #linea = pred['linea'][0]
+        #estacion = pred['estacion'][0]
+        #pronostico_afluencia = pred['pronostico_afluencia'][0]
+        #datos_a_csv = {'fecha':[fecha],'linea':[linea],'estacion':[estacion],'pronostico_afluencia':[pronostico_afluencia]} 
+        #pandas_a_csv = pd.DataFrame(data=datos_a_csv)
+        pandas_a_csv = pred
+        print("dimensiones de prediccion pandas_a_csv",pandas_a_csv.shape)
+        #pandas_a_csv.to_csv(self.output().path, index=False)
+        
+        with self.output().open('w') as output_file:
+            pandas_a_csv.to_csv(output_file,index=False)
+
+        
+        engine = create_engine('postgresql+psycopg2://postgres:12345678@database-1.cqtrfcufxibu.us-west-2.rds.amazonaws.com:5432/dpa')
+        print("ya pasó engine")
+        table_name= 'metro'
+        print(table_name)
+        scheme='predict'
+        print(scheme)
+        pred.to_sql(table_name, con=engine,schema='predict' , if_exists='append', index=False)
+        #df2.to_sql(table_name, con=engine,schema='semantic' , if_exists='replace')
+#        connection.commit()
+#        connection.close()
+        
+        #return prediction
+    
+    # Envía el output al S3 cop especificado con el nombre de output_path
+    #def output(self):
+    #    #output_path = "s3://dpaprojs3/predictionMetro_task_07_01/metro_2010-04-19.csv"#.format(self.date) #Formato del nombre para el json que entra al bucket S3
+    #    output_path = "s3://dpaprojs3/predictionMetro_task_07_01/metro_{}.csv".format(self.date) #Formato del nombre para el json que entra al bucket S3
+    #    print("en el output:",output_path)
+    #    return luigi.contrib.s3.S3Target(path=output_path)
+
+    def output(self):
+        output_path = "s3://{}/{}/metro_{}.csv". \
+            format(self.bucket, self.task_name, self.date) #Formato del nombre para el json que entra al bucket S3
+        return luigi.contrib.s3.S3Target(path=output_path)
+
+
+if __name__ == '__main__':
+    luigi.predictionMetro()
+
