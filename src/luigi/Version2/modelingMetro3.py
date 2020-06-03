cf36a1a4eb27dd94d3bef39c37a514989f5b4461
123972 <rafaelortega_itam@hotmail.com>
2020-06-02 21:11:45 -0500

    add print info

--- src/luigi/Version2/modelingMetro3.py
+++ src/luigi/Version2/modelingMetro3.py
@@ -109,6 +109,7 @@ class modelingMetro3(luigi.Task):
                                      'recall':[recall_baja,recall_normal,recall_alta]}
         
         infoUltimoModelo = pd.DataFrame(data=diccionario_de_resultados)
+        print(infoUltimoModelo)
         
         engine = create_engine('postgresql+psycopg2://postgres:12345678@database-1.cqtrfcufxibu.us-west-2.rds.amazonaws.com:5432/dpa')
         print("ya pasó engine")
